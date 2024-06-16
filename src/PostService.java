b6087c322449e1e6cdd0bd0c4b80280da1e7e77d
milcho0604 <milcho0604@gmail.com>
2024-06-16 15:47:49 +0900

    AllAuhtorInquiry, AuthorDetail, WritePost, AllPostInquiry, PostDetail 구현

--- src/PostService.java
+++ src/PostService.java
@@ -26,57 +26,19 @@ public class PostService {
         while (true) {
             System.out.println("원하시는 서비스 번호를 입력해주세요 :\n 1. 회원가입\n 2. 회원 전체 목록 조회\n 3. 회원 상세 조회\n 4. 게시글 작성\n 5. 게시글 목록 조회\n 6. 게시글 상세 조회");
             int input = sc.nextInt();
-            if (input == 1) {
+            if (input == 1) {           // 회원가입
                 SignAuthor();
-            } else if (input == 2) {
-                System.out.println("전체 회원목록을 조회합니다");
-                for (int i = 0; i < authors.size(); i++) {
-                    System.out.print("회원번호와 이메일 ");
-                    authors.get(i).inquiry();
-                }
-            } else if (input == 3) {
-                System.out.println("상세 조회 페이지입니다. 조회할 회원의 id를 입력해주세요");
-                Long id = sc.nextLong();
-                for (int i = 0; i < authors.size(); i++) {
-                    if (authors.get(i).getA_id().equals(id)) {
-                        authors.get(i).detailAuthor(id);
-                        break;
-                    }
-                }
+            } else if (input == 2) {    // 전체회원목록 조회
+                AllAuhtorInquiry();
+            } else if (input == 3) {    // 회원 상세 조회
+                AuthorDetail();
             } else if (input == 4) {
-                System.out.println("게시글 작성을 위해 이메일 인증을 진행합니다.");
-                String em = sc.next();
+                WritePost();            // 게시글 작성
 
-                for (int i = 0; i < authors.size(); i++) {
-                    if (authors.get(i).getEmail().equals(em)) {
-                        System.out.println("인증에 성공했습니다.");
-                        System.out.println("작성할 게시글의 제목을 입력해주세요");
-                        String ti = sc.next();
-                        System.out.println("작성할 게시글의 내용을 입력해주세요");
-                        String con = sc.next();
-                        // 여기서 이메일을 Author 객체로 받아야....
-                        Post post = new Post(ti, con, authors.get(i));
-                        posts.add(post);
-                        System.out.println("작성완료");
-                    } else {
-                        System.out.println("인증에 실패했습니다. 처음 화면으로 돌아갑니다");
-                        continue;
-                    }
-                }
             } else if (input == 5) {
-                System.out.println("전체 게시글 목록을 조회합니다.");
-                for (int i = 0; i < posts.size(); i++) {
-                    posts.get(i).pInqu();
-                }
+                AllPostInquiry();       // 전체게시글 조회
             } else if (input == 6) {
-                System.out.println("상세 조회 페이지입니다. 조회할 게시글의 id를 입력해주세요");
-                System.out.println("게시글의 id, 제목, 저자의 정보(이름 || 이메일 || id)가 출력됩니다.");
-                Long id = sc.nextLong();
-                for (int i = 0; i < posts.size(); i++) {
-                    if (posts.get(i).getP_id().equals(id)) {
-                        posts.get(i).detailPost(id);
-                    }
-                }
+                PostDetail();              // 게시글 상세 조회
 
             }
         }
@@ -88,6 +50,60 @@ public class PostService {
         String password = sc.nextLine();
         Author author = new Author(name, email, password);
         authors.add(author);
+    }
+    public static void AllAuhtorInquiry(){
+        System.out.println("전체 회원목록을 조회합니다");
+        for (int i = 0; i < authors.size(); i++) {
+            System.out.print("회원번호와 이메일 ");
+            authors.get(i).authorInquiry();
+        }
+    }
+    public static void AuthorDetail(){
+        System.out.println("상세 조회 페이지입니다. 조회할 회원의 id를 입력해주세요");
+        Long id = sc.nextLong();
+        for (int i = 0; i < authors.size(); i++) {
+            if (authors.get(i).getA_id().equals(id)) {
+                authors.get(i).detailAuthor(id);
+                break;
+            }
+        }
+    }
+    public static void WritePost(){
+        System.out.println("게시글 작성을 위해 이메일 인증을 진행합니다.");
+        String em = sc.next();
+
+        for (int i = 0; i < authors.size(); i++) {
+            if (authors.get(i).getEmail().equals(em)) {
+                System.out.println("인증에 성공했습니다.");
+                System.out.println("작성할 게시글의 제목을 입력해주세요");
+                String ti = sc.next();
+                System.out.println("작성할 게시글의 내용을 입력해주세요");
+                String con = sc.next();
+                // 여기서 이메일을 Author 객체로 받아야....
+                Post post = new Post(ti, con, authors.get(i));
+                posts.add(post);
+                System.out.println("작성완료");
+            } else {
+                System.out.println("인증에 실패했습니다. 처음 화면으로 돌아갑니다");
+                continue;
+            }
+        }
+    }
+    public static void AllPostInquiry(){
+        System.out.println("전체 게시글 목록을 조회합니다.");
+        for (int i = 0; i < posts.size(); i++) {
+            posts.get(i).postInquiry();
+        }
+    }
+    public static void PostDetail(){
+        System.out.println("상세 조회 페이지입니다. 조회할 게시글의 id를 입력해주세요");
+        System.out.println("게시글의 id, 제목, 저자의 정보(이름 || 이메일 || id)가 출력됩니다.");
+        Long id = sc.nextLong();
+        for (int i = 0; i < posts.size(); i++) {
+            if (posts.get(i).getP_id().equals(id)) {
+                posts.get(i).detailPost(id);
+            }
+        }
     }
 
 }
@@ -133,7 +149,7 @@ class Author {
         return password;
     }
 
-    public void inquiry() {      // 전체 조회
+    public void authorInquiry() {      // 전체 조회
         for (int i = 0; i < author_id; i++) {
             System.out.println("id는 " + a_id + "입니다. " + "이메일은 " + email + "입니다.");
         }
@@ -183,7 +199,7 @@ class Post {
         return author;
     }
 
-    public void pInqu() {
+    public void postInquiry() {
         for (int i = 0; i < post_id; i++) {
             System.out.println("id는 " + p_id + "입니다. " + "제목은 " + title + "입니다.");
         }
